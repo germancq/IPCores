4c4e8a8aaa2e27ad4bf74e903a77f0e634b4a927
germancq <germancq@gmail.com>
2020-03-09 12:56:05 +0100

    cleaned cocotb files and added to gitignore

--- block_ciphers/present_cipher/cocotb_files/sim_build/Vtop__ALLcls.d
+++
@@ -1,8 +0,0 @@
-Vtop__ALLcls.o: Vtop__ALLcls.cpp Vtop.cpp Vtop.h \
-  /usr/local/Cellar/verilator/4.028/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/4.028/share/verilator/include/verilatedos.h \
-  Vtop__Dpi.h \
-  /usr/local/Cellar/verilator/4.028/share/verilator/include/vltstd/svdpi.h \
-  Vtop__Syms.h \
-  /usr/local/Cellar/verilator/4.028/share/verilator/include/verilated_dpi.h \
-  /usr/local/Cellar/verilator/4.028/share/verilator/include/verilated_sym_props.h
