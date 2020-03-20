8825eb991521ba8e80c317725c77c901baca1e72
germancq <germancq@gmail.com>
2020-03-20 13:53:40 +0100

    hirose present performance mode updated

---
+++ hash_functions/hirose_present/Hardware_performance_files/microSD_script_files/gen_results.py
@@ -0,0 +1,135 @@
+'''
+ # @ Author: German Cano Quiveu, germancq@dte.us.es
+ # @ Create Time: 2020-03-17 16:43:12
+ # @ Modified by: Your name
+ # @ Modified time: 2020-03-17 16:43:17
+ # @ Description:
+ '''
+
+import sys
+import os
+import itertools
+import xlwt
+import time
+
+
+BLOCK_SIZE = 512
+NUM_BLOCK_TEST = 0x00100000
+SIGNATURE = 0xAABBCCDD
+RESULTS_OFFSET = 0x0
+
+def create_fields(sheet1):
+    sheet1.write(0,1,'text')
+    sheet1.write(0,2,'expected_value')
+    sheet1.write(0,3,'ciphertext_100MHz')
+    sheet1.write(0,4,'error_100MHz')
+    sheet1.write(0,5,'HW_time_ns_100Mhz')
+    sheet1.write(0,6,'ciphertext_200MHz')
+    sheet1.write(0,7,'error_200MHz')
+    sheet1.write(0,8,'HW_time_ns_200Mhz')
+    sheet1.write(0,9,'ciphertext_400MHz')
+    sheet1.write(0,10,'error_400MHz')
+    sheet1.write(0,11,'HW_time_ns_400Mhz')
+
+def read_params_from_sd(block_n,micro_sd):
+    micro_sd.seek(BLOCK_SIZE*block_n)
+    signature = int.from_bytes(micro_sd.read(4),byteorder='big')
+    param_0 = int.from_bytes(micro_sd.read(8),byteorder='little')
+    expected_result = int.from_bytes(micro_sd.read(16),byteorder='little')
+    
+    
+    
+    
+    result_100 = int.from_bytes(micro_sd.read(16),byteorder='little')
+    exec_time_100 = int.from_bytes(micro_sd.read(8),byteorder='little')
+    result_200 = int.from_bytes(micro_sd.read(16),byteorder='little')
+    exec_time_200 = int.from_bytes(micro_sd.read(8),byteorder='little')
+    result_400 = int.from_bytes(micro_sd.read(16),byteorder='little')
+    exec_time_400 = int.from_bytes(micro_sd.read(8),byteorder='little')
+    #print(hex(result))
+    
+    return (signature,
+            param_0,
+            result_100,
+            result_200,
+            result_400,
+            expected_result,
+            exec_time_100,
+            exec_time_200,
+            exec_time_400)
+
+
+def write_params(sheet1, params , i):
+
+    text = params[1]
+    result_100 = params[2]
+    result_200 = params[3]
+    result_400 = params[4]
+    hw_time_100 = int(calculated_time_in_ns(params[6],100))
+    hw_time_200 = int(calculated_time_in_ns(params[7],100))
+    hw_time_400 = int(calculated_time_in_ns(params[8],100))
+    print('/////////////////////////')
+    print(hex(params[2]))
+    print(hex(params[3]))
+    print(hex(params[4]))
+    print(hex(params[6]))
+    print(hex(params[7]))
+    print(hex(params[8]))
+    print('/////////////////////////')
+    error_100 = 0
+    error_200 = 0
+    error_400 = 0
+
+    if(result_100 != params[5]):
+        error_100 = 1
+
+    if(result_200 != params[5]):
+        error_200 = 1
+
+    if(result_400 != params[5]):
+        error_400 = 1        
+    
+    sheet1.write(i,1,hex(text))
+    sheet1.write(i,2,hex(params[5]))
+    sheet1.write(i,3,hex(result_100))
+    sheet1.write(i,4,hex(error_100))
+    sheet1.write(i,5,hw_time_100)
+    sheet1.write(i,6,hex(result_200))
+    sheet1.write(i,7,hex(error_200))
+    sheet1.write(i,8,hw_time_200)
+    sheet1.write(i,9,hex(result_400))
+    sheet1.write(i,10,hex(error_400))
+    sheet1.write(i,11,hw_time_400)
+
+    
+    return i+1
+
+
+def calculated_time_in_ns(time_units,base_clk=100):
+    clk_counter = base_clk#get_clk_speed_from_factor(div_clk)
+    return time_units * (1000/(clk_counter))
+    
+
+def gen_calc(micro_sd):
+    wb = xlwt.Workbook()
+    sheet1 = wb.add_sheet("Hoja_1")
+    create_fields(sheet1)
+    valid_signature = 1
+    i = 1
+    while valid_signature == 1 :
+        params = read_params_from_sd(NUM_BLOCK_TEST+i-1,micro_sd)
+        #print(params[0])
+        if params[0] != SIGNATURE:
+            break
+        i = write_params(sheet1,params,i)
+
+
+    wb.save('results_new.xls')
+
+def main():
+    with open(sys.argv[1],"rb") as micro_sd:
+        gen_calc(micro_sd)
+
+
+if __name__ == "__main__":
+    main()
---
+++ hash_functions/hirose_present/Hardware_performance_files/rtl/top.sv
@@ -0,0 +1,85 @@
+/**
+ * @ Author: German Cano Quiveu, germancq@dte.us.es
+ * @ Create Time: 2020-03-20 13:31:52
+ * @ Modified by: Your name
+ * @ Modified time: 2020-03-20 13:39:39
+ * @ Description:
+ */
+
+module top(
+    input sys_clk_pad_i,
+    input rst,
+
+    output cs,
+    output sclk,
+    output mosi,
+    input miso,
+    output SD_RESET,
+    output SD_DAT_1,
+    output SD_DAT_2,
+
+    output [6:0] seg,
+    output [7:0] AN,
+
+    input [1:0] switch_i,
+    output [15:0] leds_o
+);
+
+
+localparam c = 64'h1234567812345678;
+
+  assign SD_RESET = 1'b0;
+  assign SD_DAT_1 = 1'b1;
+  assign SD_DAT_2 = 1'b1;
+
+
+logic [31:0] debug_data;
+  display #(.N(32),.CLK_HZ(100000000)) display_inst(
+    .clk(sys_clk_pad_i),
+    .rst(rst),
+    .din(debug_data),
+    .an(AN),
+    .seg(seg)
+  );
+
+logic [63:0] plaintext_uut;
+logic rst_uut;
+logic end_uut;
+logic [127:0] hash_o_uut;
+logic clk_uut;
+
+
+autotest_module #(
+  .INPUT_SIZE_1(64),
+  .OUTPUT_SIZE(128)
+) autotest_impl(
+    .clk(sys_clk_pad_i),
+    .rst(rst),
+
+    .cs(cs),
+    .sclk(sclk),
+    .mosi(mosi),
+    .miso(miso),
+
+    .err_uut(1'b0),
+    .clk_uut(clk_uut),
+    .rst_uut(rst_uut),
+    .input_to_UUT_1(plaintext_uut),
+    .end_uut(end_uut),
+    .output_from_UUT(hash_o_uut),
+    
+    .sw_debug(switch_i),
+    .debug(debug_data)
+);
+
+
+hirose_present_wrapper #(.DATA_WIDTH(64)) hirose_present_wrapper_impl(
+    .clk(clk_uut),
+    .rst(rst_uut),
+    .plaintext(plaintext_uut),
+    .c(c),
+    .hash_output(hash_o_uut),
+    .end_signal(end_uut)
+);
+
+endmodule : top
