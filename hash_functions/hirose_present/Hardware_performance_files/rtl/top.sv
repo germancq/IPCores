7b1d7e027d70e39a49ea2680cab79229e0dfb50d
germancq <germancq@gmail.com>
2021-04-06 21:23:43 +0200

    updated hirose performance  mode

--- hash_functions/hirose_present/Hardware_performance_files/rtl/top.sv
+++ hash_functions/hirose_present/Hardware_performance_files/rtl/top.sv
@@ -1,14 +1,15 @@
 /**
  * @ Author: German Cano Quiveu, germancq@dte.us.es
  * @ Create Time: 2020-03-20 13:31:52
- * @ Modified by: Your name
- * @ Modified time: 2020-03-20 13:39:39
+ * @ Modified by: German Cano Quiveu, germancq@dte.us.es
+ * @ Modified time: 2021-04-06 21:20:33
  * @ Description:
  */
 
 module top(
     input sys_clk_pad_i,
     input rst,
+    input start,
 
     output cs,
     output sclk,
@@ -55,6 +56,7 @@ autotest_module #(
 ) autotest_impl(
     .clk(sys_clk_pad_i),
     .rst(rst),
+    .start(start),
 
     .cs(cs),
     .sclk(sclk),
