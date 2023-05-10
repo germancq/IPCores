2f0bd986188d8cc89008082d0fed53224fdb4d0a
germancq <germancq@gmail.com>
2023-05-10 14:32:28 +0200

    LEA wip

--- block_ciphers/LEA/hdl_code/LEA.sv
+++ block_ciphers/LEA/hdl_code/LEA.sv
@@ -2,9 +2,20 @@
  * @ Author: German Cano Quiveu, germancq
  * @ Create Time: 2023-05-04 16:06:30
  * @ Modified by: German Cano Quiveu, germancq
- * @ Modified time: 2023-05-04 16:52:32
+ * @ Modified time: 2023-05-10 14:32:03
  * @ Description:
  */
+
+package common_functions;
+
+    function logic[31:0] order_word(input logic[31:0] word);
+        return {word[7:0],word[15:8],word[23:16],word[31:24]};
+    endfunction : order_word
+    
+endpackage : common_functions
+ 
+
+import common_functions::*;
 
 module LEA #(
     parameter KEY_LEN = 128
@@ -19,6 +30,13 @@ module LEA #(
     output end_signal
 
 );
+    key_schedule #(.KEY_LEN(KEY_LEN)) impl(
+        .clk(clk),
+        .rst(rst),
+        .key(key),
+        .roundkeys(),
+        .end_key_generation()
+    );
     
 endmodule : LEA
 
@@ -45,12 +63,24 @@ module key_schedule #(
     assign cte[7] = 32'he5c40957;
 
     logic [KEY_LEN-1:0] key_reorder;
-
-
+    generate
+        genvar i;
+        for (i = 0;i<(KEY_LEN>>3);i++) begin
+            assign key_reorder[31+(i<<5):(i<<5)] = order_word(key[31+(i<<5):(i<<5)]);
+        end
+    endgenerate
+    
     // idle, espera start o reset a 0
     // check final de cuenta
     // crea T's
     //almacena RKi
     //vuelve a check final
+    always_comb begin
+        
+        
+    end    
+    
 
 endmodule: key_schedule
+
+
