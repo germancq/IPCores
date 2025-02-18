6db58742fdf22357b614dc3984ec193ce834ed84
germancq <germancq@gmail.com>
2025-02-18 13:45:37 +0100

    updating cores, kdf hirose

--- HMAC/hash_iter/cocotb_files/hmac_spongent_iter_test_docker.py
+++ HMAC/hash_iter/cocotb_files/hmac_spongent_iter_test_docker.py
@@ -128,10 +128,7 @@ async def execution_test(dut, msg, len_msg, hmac_impl):
         # print(hex(dut.feed_data_hash.value))
 
         while dut.busy.value == 1:
-            print(hex(dut.current_state.value))
-            print(dut.busy.value)
             await n_cycles_clock(dut, 1)
-            print("ciclo")
 
         print("busy end")
         hmac_impl.feed_data(data_chunk)
