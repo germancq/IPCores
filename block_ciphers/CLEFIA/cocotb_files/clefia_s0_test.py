37e23fa79b9c58d4121d9826769163adc652c6fd
germancq <germancq@gmail.com>
2025-02-24 11:23:14 +0100

    clefia hdl, s0 function

---
+++ block_ciphers/CLEFIA/cocotb_files/clefia_s0_test.py
@@ -0,0 +1,35 @@
+import os
+import random
+import sys
+
+import clefia
+import cocotb
+from cocotb.clock import Clock
+from cocotb.regression import TestFactory
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
+
+
+def setup_block_cipher(dut, x):
+    dut.x.value = x
+
+
+@cocotb.test()
+async def test(dut, index=0):
+
+    x = random.getrandbits(8)
+
+    clefia_sw = clefia.CLEFIA()
+    expected_result = clefia_sw.S0(x)
+    setup_block_cipher(dut, x)
+    await Timer(10, unit="ns")
+
+    assert (
+        dut.y.value == expected_result
+    ), f"ERROR, EXPECTED value should be {hex(expected_result)}, however hdl value is {hex(dut.y.value)}"
+
+
+n = 10
+factory = TestFactory(test)
+
+factory.add_option("index", range(0, n))
+factory.generate_tests()
