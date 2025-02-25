da5b29cf5756c3938aefdae734dcdb853a17726a
germancq <germancq@gmail.com>
2025-02-25 16:45:36 +0100

    clefia hdl, gfn function

--- block_ciphers/CLEFIA/cocotb_files/gfn_test.py
+++ block_ciphers/CLEFIA/cocotb_files/gfn_test.py
@@ -24,7 +24,7 @@ def setup_block_cipher(dut, rk, blk_i):
     print("setup block cipher")
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD).start())
     dut.rst.value = 0
-    print(dut)
+    print(dut.block_i.value)
     for i in range(0, dut.d.value):
         dut.block_i[i].value = blk_i[i]
         print("valor")
