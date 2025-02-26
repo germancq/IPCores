6ccbd090d55002d91209bf9f1f78848d1d620948
germancq <germancq@gmail.com>
2025-02-26 13:03:21 +0100

    clefia hdl, gfn function

--- block_ciphers/CLEFIA/cocotb_files/gfn_test.py
+++ block_ciphers/CLEFIA/cocotb_files/gfn_test.py
@@ -95,7 +95,7 @@ async def step2_1_test(dut, clefia_sw, blk_i, rk, counter_value):
     print(hex(dut.f0_x_input[0].value))
     print(hex(expected_f0_x_input_0))
     print(expected_f0_output_0)
-    print(hex(expected_f0_output_0))
+    print(hex(expected_f0_output_0[0]))
     assert hex(dut.f0_x_input[0].value) == hex(
         expected_f0_x_input_0
     ), f"ERROR IN STEP_2_1 f0_x_input expected = {hex(expected_f0_x_input_0)}, calculated = {hex(dut.f0_x_input[0].value)}"
