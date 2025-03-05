b16e6d733f5fa2dc66551f196477ed206ea34630
germancq <germancq@gmail.com>
2025-03-05 18:43:57 +0100

    clefia hdl, wip key_schedule

--- block_ciphers/CLEFIA/hdl_code/key_schedule.sv
+++ block_ciphers/CLEFIA/hdl_code/key_schedule.sv
@@ -19,16 +19,16 @@ module key_schedule #(
 
   localparam N_RK = 36 - ((KEY_LEN - 128) >> (3));
 
-  logic [31:0] key_l[3:0];
-  assign key_l[0] = key[31:0];
-  assign key_l[1] = key[63:32];
-  assign key_l[2] = key[95:64];
-  assign key_l[3] = key[127:96];
   logic [31:0] key_r[3:0];
-  assign key_r[0] = KEY_LEN == 128 ? 0 : key[159:128];
-  assign key_r[1] = KEY_LEN == 128 ? 0 : key[191:160];
-  assign key_r[2] = KEY_LEN == 128 ? 0 : (KEY_LEN == 192 ? ~key[31:0] : key[223:192]);
-  assign key_r[3] = KEY_LEN == 128 ? 0 : (KEY_LEN == 192 ? ~key[63:32] : key[255:224]);
+  assign key_r[0] = key[31:0];
+  assign key_r[1] = key[63:32];
+  assign key_r[2] = key[95:64];
+  assign key_r[3] = key[127:96];
+  logic [31:0] key_l[3:0];
+  assign key_l[0] = KEY_LEN == 128 ? 0 : key[159:128];
+  assign key_l[1] = KEY_LEN == 128 ? 0 : key[191:160];
+  assign key_l[2] = KEY_LEN == 128 ? 0 : (KEY_LEN == 192 ? ~key[31:0] : key[223:192]);
+  assign key_l[3] = KEY_LEN == 128 ? 0 : (KEY_LEN == 192 ? ~key[63:32] : key[255:224]);
 
   function [127:0] doubleSwap(input [127:0] block_i);
     return {block_i[120:64], block_i[127:121], block_i[6:0], block_i[63:7]};
@@ -80,7 +80,6 @@ module key_schedule #(
   logic [31:0] gfn4_block_o[3:0];
   logic gfn4_end_signal;
   logic [31:0] gfn4_round_keys[23:0];
-  logic [31:0] gfn4_block_i[3:0];
 
   genvar i;
   generate
@@ -406,6 +405,8 @@ module key_schedule #(
     end
   end
 endmodule
+
+
 
 
 
