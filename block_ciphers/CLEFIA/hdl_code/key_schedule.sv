cf8ad9231f19f4dc8b044301f291014f6fefe766
germancq <germancq@gmail.com>
2025-03-05 15:49:12 +0100

    clefia hdl, wip key_schedule

--- block_ciphers/CLEFIA/hdl_code/key_schedule.sv
+++ block_ciphers/CLEFIA/hdl_code/key_schedule.sv
@@ -34,37 +34,37 @@ module key_schedule #(
     return {block_i[120:64], block_i[127:121], block_i[6:0], block_i[63:7]};
   endfunction
 
-  //  logic [31:0] number_rounds;
-  //  generate
-  //    case (KEY_LEN)
-  //      128: begin
-  //        assign number_rounds = 8;
-  //      end
-  //      192: begin
-  //        assign number_rounds = 10;
-  //      end
-  //      default:
-  //      begin
-  //        assign number_rounds = 12;
-  //      end
-  //    endcase
-  //  endgenerate
-  //
-  //  //counter for rounds
-  //  logic up_rounds_counter;
-  //  logic rst_rounds_counter;
-  //  logic [31:0] din_rounds_counter;
-  //  logic [31:0] dout_rounds_counter;
-  //  counter #(
-  //      .DATA_WIDTH(32)
-  //  ) rounds_counter (
-  //      .clk (clk),
-  //      .rst (rst_rounds_counter),
-  //      .up  (up_rounds_counter),
-  //      .down(1'b0),
-  //      .din (din_rounds_counter),
-  //      .dout(dout_rounds_counter)
-  //  );
+  logic [31:0] number_rounds;
+  generate
+    case (KEY_LEN)
+      128: begin
+        assign number_rounds = 8;
+      end
+      192: begin
+        assign number_rounds = 10;
+      end
+      default:
+      begin
+        assign number_rounds = 12;
+      end
+    endcase
+  endgenerate
+
+  //counter for rounds
+  logic up_rounds_counter;
+  logic rst_rounds_counter;
+  logic [31:0] din_rounds_counter;
+  logic [31:0] dout_rounds_counter;
+  counter #(
+      .DATA_WIDTH(32)
+  ) rounds_counter (
+      .clk (clk),
+      .rst (rst_rounds_counter),
+      .up  (up_rounds_counter),
+      .down(1'b0),
+      .din (din_rounds_counter),
+      .dout(dout_rounds_counter)
+  );
   //
   //  //CON
   //  logic [31:0] CON_128[59:0];
@@ -389,6 +389,10 @@ module key_schedule #(
   //    end
   //  end
 endmodule
+
+
+
+
 
 
 
