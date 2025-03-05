6c7e92834d8c2af1ee9d6b334cc09b4a58ef9c94
germancq <germancq@gmail.com>
2025-03-05 18:10:14 +0100

    clefia hdl, wip key_schedule

--- block_ciphers/CLEFIA/cocotb_files/key_schedule_test.py
+++ block_ciphers/CLEFIA/cocotb_files/key_schedule_test.py
@@ -56,8 +56,8 @@ async def gfn_test(dut, expected_l_value):
 
     print(dut.gfn4_end_signal.value)
     print(dut.gfn8_end_signal.value)
-    while dut.gfn4_end_signal.value == 0 or dut.gfn8_end_signal == 0:
-        print(hex(dut.gfn_inst_8.dout.dout_rounds_counter.value))
+    while (dut.gfn4_end_signal.value == 0) or (dut.gfn8_end_signal == 0):
+        print(hex(dut.gfn_inst_8.dout_rounds_counter.value))
         print(hex(dut.gfn_inst.dout_rounds_counter.value))
         await n_cycles_clock(dut, 1)
 
