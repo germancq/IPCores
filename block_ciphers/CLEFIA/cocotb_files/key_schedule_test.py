c138e5537fac5ed3f1b0795cf9b4c4d323235f13
germancq <germancq@gmail.com>
2025-03-07 14:52:21 +0100

    clefia hdl, wip key_schedule

--- block_ciphers/CLEFIA/cocotb_files/key_schedule_test.py
+++ block_ciphers/CLEFIA/cocotb_files/key_schedule_test.py
@@ -188,6 +188,7 @@ async def check_odd_test(dut, expected_L, expected_T, expected_counter_value):
 
 async def gen_rk_test(dut, expected_RK, expected_counter_value):
     print("gen_rk_test")
+    print(dut.N_RK.value)
     await n_cycles_clock(dut, 1)
     assert (
         dut.current_state.value == dut.GEN_RK.value
