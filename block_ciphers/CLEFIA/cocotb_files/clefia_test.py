8cf16371a70c97b2ee536ee4f2afb73d9de1609f
germancq <germancq@gmail.com>
2025-03-10 20:16:25 +0100

    clefia hdl

--- block_ciphers/CLEFIA/cocotb_files/clefia_test.py
+++ block_ciphers/CLEFIA/cocotb_files/clefia_test.py
@@ -41,9 +41,6 @@ async def test(dut, index=0):
     key = random.getrandbits(dut.KEY_LEN.value)
     plaintext = random.getrandbits(128)
 
-    key = 0xFFEEDDCCBBAA99887766554433221100
-    plaintext = 0x000102030405060708090A0B0C0D0E0F
-
     setup_dut(dut, key, plaintext)
 
     expected_wk, expected_rk = clefia_sw.key_schedule(key, dut.KEY_LEN.value)
