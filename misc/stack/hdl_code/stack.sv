42ef0fcde89c107771631f94091b99a464bcefe6
germancq <germancq@gmail.com>
2025-04-06 12:21:01 +0200

    stack sv in misc modified

--- misc/stack/hdl_code/stack.sv
+++ misc/stack/hdl_code/stack.sv
@@ -48,3 +48,49 @@ module stack_fifo
 
 
 endmodule : stack_fifo
+
+
+module stack_fifo_sp 
+#(parameter DEPTH = 8, 
+  parameter WIDTH = 8
+)
+(
+    input clk,
+    input rst,
+    input [WIDTH-1:0] data_i,
+    output logic [WIDTH-1:0] data_o,
+    input push,
+    input pop,
+    output empty,
+    output full,
+    output sp_index,
+);
+
+    logic [$clog2(DEPTH):0] sp;
+    logic [WIDTH-1:0]  stack [DEPTH-1:0];
+
+    assign empty = (sp == 0 ? 1 : 0);
+    assign full = (sp == DEPTH ? 1 : 0);
+    assign sp_index = sp;
+
+    always_ff @(posedge clk) begin
+        if (rst)begin
+            sp <= 0;
+            data_o <= 0;
+        end
+        else if(push) begin
+            if (full == 0) begin
+                sp <= sp + 1;
+                stack[sp] <= data_i;
+            end
+        end
+        else if(pop) begin
+            if (empty == 0) begin
+                sp <= sp - 1;
+                data_o <= stack[sp-1];
+            end
+        end
+    end
+
+
+endmodule : stack_fifo
