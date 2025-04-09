148aaa6988d108dbb0ebc9336a172b6b219ade26
germancq <germancq@gmail.com>
2025-04-09 14:26:43 +0200

    updated stack.sv

--- misc/stack/hdl_code/stack.sv
+++ misc/stack/hdl_code/stack.sv
@@ -6,11 +6,10 @@
  * @ Description:
  */
 
-module stack_fifo 
-#(parameter DEPTH = 8, 
-  parameter WIDTH = 8
-)
-(
+module stack_fifo #(
+    parameter DEPTH = 8,
+    parameter WIDTH = 8
+) (
     input clk,
     input rst,
     input [WIDTH-1:0] data_i,
@@ -21,40 +20,37 @@ module stack_fifo
     output full
 );
 
-    logic [$clog2(DEPTH):0] sp;
-    logic [WIDTH-1:0]  stack [DEPTH-1:0];
+  logic [$clog2(DEPTH):0] sp;
+  logic [WIDTH-1:0] stack[DEPTH-1:0];
 
-    assign empty = (sp == 0 ? 1 : 0);
-    assign full = (sp == DEPTH ? 1 : 0);
+  assign empty = (sp == 0 ? 1 : 0);
+  assign full  = (sp == DEPTH ? 1 : 0);
 
-    always_ff @(posedge clk) begin
-        if (rst)begin
-            sp <= 0;
-            data_o <= 0;
-        end
-        else if(push) begin
-            if (full == 0) begin
-                sp <= sp + 1;
-                stack[sp] <= data_i;
-            end
-        end
-        else if(pop) begin
-            if (empty == 0) begin
-                sp <= sp - 1;
-                data_o <= stack[sp-1];
-            end
-        end
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      sp <= 0;
+      data_o <= 0;
+    end else if (push) begin
+      if (full == 0) begin
+        sp <= sp + 1;
+        stack[sp] <= data_i;
+      end
+    end else if (pop) begin
+      if (empty == 0) begin
+        sp <= sp - 1;
+        data_o <= stack[sp-1];
+      end
     end
+  end
 
 
 endmodule : stack_fifo
 
 
-module stack_fifo_sp 
-#(parameter DEPTH = 8, 
-  parameter WIDTH = 8
-)
-(
+module stack_fifo_sp #(
+    parameter DEPTH = 8,
+    parameter WIDTH = 8
+) (
     input clk,
     input rst,
     input [WIDTH-1:0] data_i,
@@ -63,34 +59,33 @@ module stack_fifo_sp
     input pop,
     output empty,
     output full,
-    output sp_index,
+    output sp_index
 );
 
-    logic [$clog2(DEPTH):0] sp;
-    logic [WIDTH-1:0]  stack [DEPTH-1:0];
+  logic [$clog2(DEPTH):0] sp;
+  logic [WIDTH-1:0] stack[DEPTH-1:0];
 
-    assign empty = (sp == 0 ? 1 : 0);
-    assign full = (sp == DEPTH ? 1 : 0);
-    assign sp_index = sp;
+  assign empty = (sp == 0 ? 1 : 0);
+  assign full = (sp == DEPTH ? 1 : 0);
+  assign sp_index = sp;
 
-    always_ff @(posedge clk) begin
-        if (rst)begin
-            sp <= 0;
-            data_o <= 0;
-        end
-        else if(push) begin
-            if (full == 0) begin
-                sp <= sp + 1;
-                stack[sp] <= data_i;
-            end
-        end
-        else if(pop) begin
-            if (empty == 0) begin
-                sp <= sp - 1;
-                data_o <= stack[sp-1];
-            end
-        end
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      sp <= 0;
+      data_o <= 0;
+    end else if (push) begin
+      if (full == 0) begin
+        sp <= sp + 1;
+        stack[sp] <= data_i;
+      end
+    end else if (pop) begin
+      if (empty == 0) begin
+        sp <= sp - 1;
+        data_o <= stack[sp-1];
+      end
     end
+  end
 
 
-endmodule : stack_fifo
+endmodule : stack_fifo_sp
+
