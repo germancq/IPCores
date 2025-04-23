91e9d73a387767e8ad20a80db07081bb673221db
germancq <germancq@gmail.com>
2025-04-23 16:07:41 +0200

    update stack.sv

--- misc/stack/hdl_code/stack.sv
+++ misc/stack/hdl_code/stack.sv
@@ -59,7 +59,7 @@ module stack_fifo_sp #(
     input pop,
     output empty,
     output full,
-    output sp_index
+    output [$clog2(DEPTH):0] sp_index
 );
 
   logic [$clog2(DEPTH):0] sp;
@@ -89,3 +89,54 @@ module stack_fifo_sp #(
 
 endmodule : stack_fifo_sp
 
+module stack #(
+    parameter FIFO_LIFO = 0,
+    parameter DEPTH = 8,
+    parameter WIDTH = 8
+) (
+    input clk,
+    input rst,
+    input [WIDTH-1:0] data_i,
+    output logic [WIDTH-1:0] data_o,
+    input push,
+    input pop,
+    output empty,
+    output full,
+    output [$clog2(DEPTH):0] sp_index
+);
+
+  logic [$clog2(DEPTH):0] sp;
+  logic [WIDTH-1:0] stack[DEPTH-1:0];
+  assign sp_index = sp;
+
+  assign empty = (sp == 0 ? 1 : 0);
+  assign full = (sp == DEPTH ? 1 : 0);
+
+  logic [15:0] i;
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      sp <= 0;
+      data_o <= 0;
+    end else if (push) begin
+      if (full == 0) begin
+        sp <= sp + 1;
+        stack[sp] <= data_i;
+      end
+    end else if (pop) begin
+      if (empty == 0) begin
+        if (FIFO_LIFO == 0) begin
+          sp <= sp - 1;
+          data_o <= stack[sp-1];
+        end else begin
+          sp <= sp - 1;
+          data_o <= stack[0];
+          for (i = 0; i < DEPTH - 1; i++) begin
+            stack[i] <= stack[i+1];
+          end
+        end
+      end
+    end
+  end
+
+
+endmodule : stack
