feb9d0419320b3c5be62a4dc3bcc1efe91d562e2
germancq <germancq@gmail.com>
2025-04-24 13:59:01 +0200

    update counter.sv

--- misc/counters/hdl_code/counter.sv
+++ misc/counters/hdl_code/counter.sv
@@ -4,33 +4,60 @@
  * @Last Modified by: German Cano Quiveu, germancq@gmail.com
  * @Last Modified time: 2019-09-22 22:37:28
  */
-module counter
-    #(parameter DATA_WIDTH = 8)
-    (
-        input clk,
-        input rst,
-        input up,
-        input down,
-        input [DATA_WIDTH-1 : 0] din,
-        output logic [DATA_WIDTH -1 : 0] dout
-    );
+module counter #(
+    parameter DATA_WIDTH = 8
+) (
+    input clk,
+    input rst,
+    input up,
+    input down,
+    input [DATA_WIDTH-1 : 0] din,
+    output logic [DATA_WIDTH -1 : 0] dout
+);
 
 
 
-always_ff @(posedge clk) 
-begin
-    if(rst == 1)
-        begin
-            dout <= din;
-        end
-    else if (up == 1) 
-        begin
-            dout <= dout + 1;
-        end    
-    else if (down == 1) 
-        begin
-            dout <= dout - 1;
-        end                
-end
+  always_ff @(posedge clk) begin
+    if (rst == 1) begin
+      dout <= din;
+    end else if (up == 1) begin
+      dout <= dout + 1;
+    end else if (down == 1) begin
+      dout <= dout - 1;
+    end
+  end
 
 endmodule : counter
+
+module counter_limit #(
+    parameter DATA_WIDTH = 8
+) (
+    input clk,
+    input rst,
+    input up,
+    input down,
+    input [DATA_WIDTH-1 : 0] din,
+    output logic [DATA_WIDTH -1 : 0] dout
+);
+
+
+
+  always_ff @(posedge clk) begin
+    if (rst == 1) begin
+      dout <= din;
+    end else if (up == 1) begin
+      if (dout == {DATA_WIDTH{1'b1}}) begin
+        dout <= dout;
+      end else begin
+        dout <= dout + 1;
+      end
+    end else if (down == 1) begin
+      if (dout == 0) begin
+        dout <= dout;
+      end else begin
+        dout <= dout - 1;
+      end
+    end
+  end
+
+endmodule : counter_limit
