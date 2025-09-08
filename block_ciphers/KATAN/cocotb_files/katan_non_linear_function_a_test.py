1e7e3835669c8e85f595c8837e77a7608d0edf19
germancq <germancq@gmail.com>
2025-09-08 17:12:41 +0200

    wip katan cocotb

---
+++ block_ciphers/KATAN/cocotb_files/katan_non_linear_function_a_test.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : katan_non_linear_function_a.py
+# Author            : German C.Quiveu <germancq@dte.us.es>
+# Date              : 08.09.2025
+# Last Modified Date: 08.09.2025
+
+import os
+import random
+import sys
+
+import cocotb
+import katan
+import numpy as np
+from cocotb.clock import Clock
+from cocotb.regression import TestFactory
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
+
+
+def setup_dut(dut, katan_values, counter_state, rk):
+    dut.x1.value = katan_values["x1"]
+    dut.x2.value = katan_values["x2"]
+    dut.x3.value = katan_values["x3"]
+    dut.x4.value = katan_values["x4"]
+    dut.x5.value = katan_values["x5"]
+    dut.L1.value = katan_values["L1"]
+    dut.ir.value = (counter_state >> 7) & 1
+    dut.rk.value = rk
+
+
+@cocotb.test()
+async def test(dut, index=0):
+
+    if dut.L1_LEN.value == 13:
+        N = 32
+    elif dut.L1_LEN.value == 19:
+        N = 48
+    else:
+        N = 64
+
+    katan_values = katan.KATAN_VALUES[N]
+    katan_cipher_sw = katan.KATAN(N, random.getrandbits(80))
+    rk = random.getrandbits(1)
+
+    print(N)
+
+    katan_values["L1"] = random.getrandbits(dut.L1_LEN.value)
+
+    katan_cipher_sw.L1 = katan_values["L1"]
+
+    for i in (0, random.randint(10, 50)):
+        katan_cipher_sw.counter.step()
+
+    counter_state = katan_cipher_sw.counter.get_state()
+
+    await Timer(10, units="ns")
+
+    setup_dut(dut, katan_values, counter_state, rk)
+
+    expected_result = katan_cipher_sw.non_linear_function_a(rk)
+
+    assert (
+        dut.result.value == expected_result
+    ), f"ERROR with result, with N = {N}, expected_value = {expected_result} and calculated = {dut.result.value}"
+
+
+n = 0x5
+factory = TestFactory(test)
+
+factory.add_option("index", range(0, n))
+factory.generate_tests()
