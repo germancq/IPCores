3a97bc4b9f31ba53b4de6900b34a2930b0acc01c
germancq <germancq@gmail.com>
2025-09-08 16:28:52 +0200

    katan cocotb wip

---
+++ block_ciphers/KATAN/hdl_code/non_linear_function_a.sv
@@ -0,0 +1,25 @@
+/**
+ * File              : non_linear_function_a.sv
+ * Author            : German C.Quiveu <germancq@dte.us.es>
+ * Date              : 08.09.2025
+ * Last Modified Date: 08.09.2025
+ * Last Modified By  : German C.Quiveu <germancq@dte.us.es>
+ */
+
+module non_linear_function_a #(
+    parameter L1_LEN = 13
+) (
+    input [$floor($clog2(L1_LEN)):0] x1,
+    input [$floor($clog2(L1_LEN)):0] x2,
+    input [$floor($clog2(L1_LEN)):0] x3,
+    input [$floor($clog2(L1_LEN)):0] x4,
+    input [$floor($clog2(L1_LEN)):0] x5,
+    input rk,
+    input ir,  //IR = counter_state[7]
+    input [L1_LEN-1:0] L1_reg,
+    output result
+);
+
+  assign result = L1_reg[x1] ^ L1_reg[x2] ^ (L1_reg[x3] & L1_reg[x4]) ^ (ir & L1_reg[x5]) ^ rk;
+
+endmodule
