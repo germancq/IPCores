690e62ba8062e0bdd17ec34d0a2d11bd35cbec2b
germancq <germancq@gmail.com>
2025-09-30 12:10:06 +0200

    katan cocotb wip

--- block_ciphers/KATAN/cocotb_files/katan_encrypt_test.py
+++ block_ciphers/KATAN/cocotb_files/katan_encrypt_test.py
@@ -89,6 +89,8 @@ async def encrypt_loop_test(dut, katan_sw):
     L1_val = (dut.block_i.value >> (katan_sw.L2)) & ((2**katan_sw.L1) - 1)
     katan_sw.L1_reg = L1_val
 
+    print("plaintext is {hex(dut.block_i.value)}")
+
     assert (
         dut.encrypt_impl.L1_reg_dout.value == katan_sw.L1_reg
     ), f"ERROR loading plaintext, expected={hex(katan_sw.L1_reg)}, calculated={hex(dut.encrypt_impl.L1_reg_dout.value)}"
