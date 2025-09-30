754baf89378e95f06493b073fccd5801e43fdc86
germancq <germancq@gmail.com>
2025-09-30 11:53:28 +0200

    katan cocotb wip

--- misc/registers/hdl_code/register.sv
+++ misc/registers/hdl_code/register.sv
@@ -8,97 +8,88 @@
 
 
 
-module register
-    #(parameter DATA_WIDTH = 8)
-    (
-        input clk,
-        input cl,
-        input w,
-        input [DATA_WIDTH-1:0] din,
-        output logic [DATA_WIDTH-1:0] dout
-    );
+module register #(
+    parameter DATA_WIDTH = 8
+) (
+    input clk,
+    input cl,
+    input w,
+    input [DATA_WIDTH-1:0] din,
+    output logic [DATA_WIDTH-1:0] dout
+);
 
 
 
-always_ff @(posedge clk) begin
+  always_ff @(posedge clk) begin
     if (cl) begin
-        dout <= { DATA_WIDTH {1'b0} };
+      dout <= {DATA_WIDTH{1'b0}};
+    end else if (w) begin
+      dout <= din;
     end
-    else if (w) begin
-        dout <= din;
-    end
-end
+  end
 
 endmodule : register
 
-module register_initial_state
-    #(parameter DATA_WIDTH = 8)
-    (
-        input clk,
-        input cl,
-        input w,
-        input [DATA_WIDTH-1:0] initial_state,
-        input [DATA_WIDTH-1:0] din,
-        output logic [DATA_WIDTH-1:0] dout
-    );
+module register_initial_state #(
+    parameter DATA_WIDTH = 8
+) (
+    input clk,
+    input cl,
+    input w,
+    input [DATA_WIDTH-1:0] initial_state,
+    input [DATA_WIDTH-1:0] din,
+    output logic [DATA_WIDTH-1:0] dout
+);
 
 
 
-always_ff @(posedge clk) begin
+  always_ff @(posedge clk) begin
     if (cl) begin
-        dout <= initial_state;
+      dout <= initial_state;
+    end else if (w) begin
+      dout <= din;
     end
-    else if (w) begin
-        dout <= din;
-    end
-end
+  end
 
 endmodule : register_initial_state
 
 
-module shift_register
-    #(parameter DATA_WIDTH = 8)
-    (
-        input clk,
-        input cl,
-        input shift_right,
-        input shift_left,
-        input load,
-        input input_bit,
-        input [DATA_WIDTH-1 : 0] din,
-        output logic output_bit,
-        output logic [DATA_WIDTH-1 : 0] dout
-    );
+module shift_register #(
+    parameter DATA_WIDTH = 8
+) (
+    input clk,
+    input cl,
+    input shift_right,
+    input shift_left,
+    input load,
+    input input_bit,
+    input [DATA_WIDTH-1 : 0] din,
+    output logic output_bit,
+    output logic [DATA_WIDTH-1 : 0] dout
+);
 
-always_ff @(posedge clk) begin
+  always_ff @(posedge clk) begin
 
-    if (cl == 1)
-        begin
-            dout <= {DATA_WIDTH{1'b0}};
-        end
-    else if(load == 1)
-        begin
-            dout <= din;
-        end
-    else if (shift_right == 1) 
-        begin
-            output_bit <= dout[0];
-            
-            dout[DATA_WIDTH-1:0] <= {input_bit,dout[DATA_WIDTH-1:1]};
-        end        
-    else if (shift_left == 1)  
-        begin
-            output_bit <= dout[DATA_WIDTH - 1];
-            dout[DATA_WIDTH-1:0] <= {dout[DATA_WIDTH-2:0],input_bit};
-        end 
-end
+    if (cl == 1) begin
+      dout <= {DATA_WIDTH{1'b0}};
+    end else if (load == 1) begin
+      dout <= din;
+    end else if (shift_right == 1) begin
+      output_bit <= dout[0];
 
-endmodule : shift_register 
+      dout[DATA_WIDTH-1:0] <= {input_bit, dout[DATA_WIDTH-1:1]};
+    end else if (shift_left == 1) begin
+      output_bit <= dout[DATA_WIDTH-1];
+      dout[DATA_WIDTH-1:0] <= {dout[DATA_WIDTH-2:0], input_bit};
+    end
+  end
+
+endmodule : shift_register
 
 module LFSR #(
-    parameter DATA_WIDTH = 6
-)
-(
+    parameter DATA_WIDTH = 6,
+    parameter LSB = 1
+) (
     input clk,
     input rst,
     input shift,
@@ -107,19 +98,24 @@ module LFSR #(
     output [DATA_WIDTH-1:0] state
 );
 
-    logic [DATA_WIDTH-1:0] state_reg;
-    logic bit_xored;
-    assign state = state_reg;
+  logic [DATA_WIDTH-1:0] state_reg;
+  logic bit_xored;
+  assign state = state_reg;
 
-    assign bit_xored = ^(state_reg & feedback_coeff[DATA_WIDTH:1]);
+  assign bit_xored = LSB==1 ? ^(state_reg & feedback_coeff[DATA_WIDTH:1]) : ^(state_reg & feedback_coeff[DATA_WIDTH-1:0]);
 
-    always_ff @(posedge clk) begin
-        if(rst == 1) begin
-            state_reg <= initial_state;
-        end
-        else if(shift == 1) begin
-            state_reg <= {state_reg[DATA_WIDTH-2:0],bit_xored};
-        end
+  always_ff @(posedge clk) begin
+    if (rst == 1) begin
+      state_reg <= initial_state;
+    end else if (shift == 1) begin
+      if (LSB == 1) begin
+        //this is the one used in twofish
+        state_reg <= {state_reg[DATA_WIDTH-2:0], bit_xored};
+      end else begin
+        //mod realized for katan
+        state_reg <= {bit_xored, state_reg[DATA_WIDTH-1:1]};
+      end
     end
+  end
 
 endmodule : LFSR
