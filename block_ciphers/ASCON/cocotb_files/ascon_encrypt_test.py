79d3dbdd4d7d0d2d9a9ca302810ce3227a71784a
germancq <germancq@gmail.com>
2025-11-05 19:40:34 +0100

    ascon cocotb encrypt

--- block_ciphers/ASCON/cocotb_files/ascon_encrypt_test.py
+++ block_ciphers/ASCON/cocotb_files/ascon_encrypt_test.py
@@ -20,10 +20,14 @@ from cocotb.triggers import FallingEdge, RisingEdge, Timer
 CLK_PERIOD = 20
 
 
-def setup_dut(dut):
+def setup_dut(dut, key, nonce, plaintext, a_data):
     cocotb.fork(Clock(dut.clk, CLK_PERIOD, "ns").start())
     dut.rst.value = 0
-    print(dut.version.value)
+    dut.start.value = 0
+    dut.key.value = key
+    dut.nonce.value = nonce
+    dut.plaintext.value = plaintext
+    dut.a_data.value = a_data
 
 
 async def rst_function_test(dut):
@@ -38,6 +42,34 @@ async def rst_function_test(dut):
     ), f"ERROR STATE IN RST, STATE={dut.current_state.value}"
 
 
+async def initial_state_test(dut, ascon_sw):
+    dut.rst.value = 0
+    await n_cycles_clock(dut, 5)
+    assert (
+        dut.current_state.value == dut.IDLE.value
+    ), f"ERROR STATE IN RST, STATE={dut.current_state.value}"
+    dut.start.value = 1
+    await n_cycles_clock(dut, 1)
+    assert (
+        dut.current_state.value == dut.INITIAL_STATE.value
+    ), f"ERROR STATE IN INITIAL_STATE, STATE={dut.current_state.value}"
+
+    await permutation_a_test(dut)
+
+    ascon_sw.get_initial_state()
+    ascon_sw.ascon_permutation(dut.a.value)
+
+    for i in range(0, 5):
+        assert (
+            dut.state_ascon_dout[i].value == ascon_sw.state_array[i]
+        ), f"ERROR in state {i} on initial_state_test, expected = {hex(ascon_sw.state_array[i])}, calculated = {hex(dut.state_ascon_dout[i].value)}"
+
+
+async def permutation_a_test(dut):
+    while dut.current_state.value != dut.ASCON_PERMUTATION_A_2:
+        await n_cycles_clock(dut, 1)
+
+
 async def n_cycles_clock(dut, n):
     for i in range(0, n):
         await RisingEdge(dut.clk)
@@ -47,11 +79,23 @@ async def n_cycles_clock(dut, n):
 @cocotb.test()
 async def test(dut, index=0):
 
-    setup_dut(dut)
+    # plaintext = random.getrandbits(dut.rate.value * 8)
+    # key = random.getrandbits(dut.k.value)
+    # nonce = random.getrandbits(128)
+    # a_data = random.getrandbits(dut.a_len.value)
+    plaintext = 0x6173636F6E
+    key = 0x54B6A187FB0A1C64925C66F205FBA5BF
+    nonce = 0xC7B7C0CECF7DE4C7A11453474216B1A3
+    a_data = 0x4153434F4E
+
+    ascon_sw = ascon_aead.ASCON_AEAD(key, nonce)
+
+    setup_dut(dut, key, nonce, plaintext, a_data)
     await rst_function_test(dut)
+    await initial_state_test(dut, ascon_sw)
 
 
-n = 0x40
+n = 0x4
 factory = TestFactory(test)
 
 factory.add_option("index", range(0, n))
