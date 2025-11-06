ae1b3ec23c9e6bf70482a84bfe349006553e580c
germancq <germancq@gmail.com>
2025-11-06 11:47:24 +0100

    ascon cocotb encrypt

--- block_ciphers/ASCON/hdl_code/encrypt.sv
+++ block_ciphers/ASCON/hdl_code/encrypt.sv
@@ -267,7 +267,7 @@ module encrypt #(
         next_state = ASSOCIATED_DATA;
       end
       ASSOCIATED_DATA: begin
-        aux_var = 0;
+        aux_var = 20;
 
         for (j = 0; j < ($floor(a_len / (rate << 3))); j++) begin
           aux_var = aux_var ^ a_data_reord[(j*(rate<<3))+:(rate<<3)];
