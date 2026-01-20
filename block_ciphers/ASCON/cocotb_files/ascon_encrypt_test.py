e64b950f3887af00bacedabee8f7c1b4b0ffa54f
germancq <germancq@gmail.com>
2026-01-20 19:53:31 +0100

    wip ascon cocotb encrypt

--- block_ciphers/ASCON/cocotb_files/ascon_encrypt_test.py
+++ block_ciphers/ASCON/cocotb_files/ascon_encrypt_test.py
@@ -286,7 +286,7 @@ async def n_cycles_clock(dut, n):
 @cocotb.test()
 async def test(dut, index=0):
 
-    plaintext = random.getrandbits(dut.len_plaintext_bits.value)
+    plaintext = random.getrandbits(dut.plaintext_len.value)
     key = random.getrandbits(dut.k.value)
     nonce = random.getrandbits(128)
     a_data = random.getrandbits(dut.a_len.value)
