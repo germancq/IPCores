8daf098e4149d1014a6cf1393ea506fcb1f1cbf8
germancq <germancq@gmail.com>
2026-01-29 20:50:46 +0100

    wip ascon cocotb encrypt

--- block_ciphers/ASCON/hdl_code/encrypt.sv
+++ block_ciphers/ASCON/hdl_code/encrypt.sv
@@ -329,7 +329,7 @@ module encrypt #(
         custom_state_ascon_din[0] = state_ascon_dout[0] ^ a_data_reord[(generic_counter_o<<6)+:64];
 
         if (rate == 16) begin
-          aux_var = a_data_reord << ((generic_counter_o) << 7);
+          aux_var = a_data_reord >> ((generic_counter_o) << 7);
 
           //custom_state_ascon_din[0] = state_ascon_dout[0] ^ a_data_reord[(generic_counter_o<<7) +: 64];
           //custom_state_ascon_din[1] = state_ascon_dout[1] ^ a_data_reord[(generic_counter_o<<7)+64 +: 64];
